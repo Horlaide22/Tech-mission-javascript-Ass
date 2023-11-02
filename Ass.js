0b59079df809269546cffb28faab67a4c0550a40
Horlaide22 <hartoke2sekinat@gmail.com>
2023-11-02 17:17:25 +0100

    edited

--- Ass.js
+++ Ass.js
@@ -1,4 +1,4 @@
-
+// Step 1: Create a function to create a contact
 function createContact(firstName, lastName, phoneNumber, emailAddress) {
   return {
     fullName: `${firstName} ${lastName}`,
@@ -7,31 +7,33 @@ function createContact(firstName, lastName, phoneNumber, emailAddress) {
   };
 }
 
-
+// Step 2: Write a function to generate a Contact List
 function createContactList() {
   const contactList = [];
 
+  // Step 3: Add at least two instances of contacts
   const contact1 = createContact(
-    "Ade",
-    "ola",
-    "09030437904",
-    "Adeola@gmail.com"
+    "John",
+    "Doe",
+    "555-123-4567",
+    "johndoe@example.com"
   );
   const contact2 = createContact(
-    "ade",
-    "nike",
-    "08029895347",
-    "adenike@gmail.com"
+    "Alice",
+    "Smith",
+    "555-987-6543",
+    "alicesmith@example.com"
   );
 
+  // Step 4: Add the created contacts to the array
   contactList.push(contact1, contact2);
 
-
+  // Step 5: Return the Contact List
   return contactList;
 }
 
-
+// Step 6: Test your function
 const myContactList = createContactList();
 
-
+// Step 7: Console log the result
 console.log("Contact List:", myContactList);
